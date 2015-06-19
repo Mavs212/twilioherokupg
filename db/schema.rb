e63b0155f0c338b6f73cfbd01306d832b4050ede
Simone Battiste-Alleyne <softe1988@gmail.com>
2015-06-19 17:10:28 +0000

    added instructor table

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20150619170941) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "instructors", force: :cascade do |t|
+    t.string   "name"
+    t.string   "number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
